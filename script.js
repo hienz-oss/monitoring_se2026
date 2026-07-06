303efca73750ae23df147d5562f61493abb5d51f
hienz-oss <hienz.awaludin@gmail.com>
2026-07-06 19:52:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1809,6 +1809,27 @@ document.addEventListener("click", (e) => {
 });
 
 /* =========================
+   GO TOP BUTTON
+========================= */
+
+const goTopBtn = document.getElementById("goTopBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        goTopBtn.classList.add("show");
+    } else {
+        goTopBtn.classList.remove("show");
+    }
+});
+
+goTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+
+/* =========================
    INIT
 ========================= */
 
